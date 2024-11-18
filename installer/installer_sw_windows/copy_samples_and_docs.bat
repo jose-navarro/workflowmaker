df2a7ed1d7ba8dd4f85e3f6fd7a5b2d2863ffdf8
José Navarro <jose.navarro@cttc.es>
2024-11-18 13:32:16 +0100

    Feature #24 implemented on Windows.

--- installer/installer_sw_windows/copy_samples_and_docs.bat
+++ installer/installer_sw_windows/copy_samples_and_docs.bat
@@ -4,5 +4,7 @@ REM
 
 @ECHO INSTALLER'S DEPLOYMENT FOLDERS: Copying user guides and data samples...
 
-COPY "..\..\docs\user guide\*.pdf"             "%1\docs\*.*"
+COPY "..\..\docs\user guide\*.pdf"                                      "%1\docs\*.*"
+COPY "..\..\samples\libraries\simple_options_file_parser\docs\*.pdf"    "%1\docs\*.*"
+COPY "..\..\samples\image_processing\docs\*.pdf"                        "%1\docs\*.*"
 COPY "..\..\samples\image_processing\sample_workflowmaker_files\*.xml"  "%1\data_samples\*.*"
